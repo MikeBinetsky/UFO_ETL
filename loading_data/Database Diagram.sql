3620cb5b2f389f387df78f036046b2d03d7b4b6a
haodong191 <hdong215@gmail.com>
2021-09-07 19:56:31 -0400

    Updated DBD

--- loading_data/Database Diagram.sql
+++ loading_data/Database Diagram.sql
@@ -3,27 +3,31 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-CREATE TABLE "UFO" (
-    "datetime" object  NOT NULL ,
-    "city" object  NOT NULL ,
-    "state" object  NOT NULL ,
-    "country" object  NOT NULL ,
-    "shape" object  NOT NULL ,
-    "latitude" object  NOT NULL ,
-    "longitude" float64  NOT NULL 
+CREATE TABLE "UFO_Data" (
+    "datetime" int  NOT NULL ,
+    "city" varchar(30)  NOT NULL ,
+    "state" varchar(30)  NOT NULL ,
+    "country" varchar(30)  NOT NULL ,
+    "shape" varchar(10)  NOT NULL ,
+    "latitude" int  NOT NULL ,
+    "longitude" int  NOT NULL 
 )
 
 GO
 
-CREATE TABLE "BIRD" (
-    "Species" object  NOT NULL ,
-    "Common_name" object  NOT NULL ,
-    "Scientific_name" object  NOT NULL ,
-    "Columbia" object  NOT NULL ,
-    "Date" object  NOT NULL ,
-    "LAT" object  NOT NULL ,
-    "LNG" object  NOT NULL 
+CREATE TABLE "Bird_Data" (
+    "Species" varchar(30)  NOT NULL ,
+    "Common_name" varchar(30)  NOT NULL ,
+    "Scientific_name" varchar(60)  NOT NULL ,
+    "location_Name" varchar(60)  NOT NULL ,
+    "Date" int  NOT NULL ,
+    "LAT" int  NOT NULL ,
+    "LNG" int  NOT NULL 
 )
 
+GO
+
+ALTER TABLE "UFO_Data" ADD CONSTRAINT "fk_UFO_Data_datetime" FOREIGN KEY("datetime")
+REFERENCES "Bird_Data" ("Date")
 GO
 
